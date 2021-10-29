e5712175094d015c362397d2b166b7e785542884
Burcu Agdere <burcuagdere@gmail.com>
2021-10-29 14:07:17 +0100

    Remove resource group creation

--- laaS/create-win-vm-azure-cli.sh
+++ laaS/create-win-vm-azure-cli.sh
@@ -1,9 +1,5 @@
 ## Creating a Windows VM with Azure CLI
 
-az group create \
-  --name "psdemo-rg" \
-  --location "northeurope"
-  
 az vm create \
   --resource-group "psdemo-rg" \
   --name "psdemo-win-cli" \
